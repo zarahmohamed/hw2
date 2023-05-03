a86d75931faedec05dab669133b3e0c22cf3b3e2
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-05-03 23:10:53 +0000

    actor table

--- db/migrate/20230503230820_create_actors.rb
+++ db/migrate/20230503230820_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration[7.0]
   def change
     create_table :actors do |t|
-      
+      t.string "name"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_230632) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_230820) do
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "year_released"
